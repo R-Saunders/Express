64ade892c1bf5246dc68fc9e41467fc674d8357f
R-Saunders <richsaunders@protonmail.com>
2022-10-21 20:37:32 +0100

    added dotenv

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 app.use(cors());
 
 const data = [
@@ -34,6 +36,7 @@ const data = [
 
 app.get('/', (request,response)=>{
   response.send('<h1>Hello World</h1>');
+  console.log(process.env.PORT);
 })
 
 app.get('/rich', (req, res)=>{
@@ -44,6 +47,6 @@ app.get('/data', (req, res)=>{
   res.json(data);
 })
 
-app.listen(8801, ()=>{
-  console.log('http://localhost:8801');
+app.listen(8989, ()=>{
+  console.log('http://localhost:8989');
 })
