02e92934b0c07fd82a5b7a0904321d9832503e9f
R-Saunders <richsaunders@protonmail.com>
2022-10-27 21:18:30 +0100

    added post and fs

--- index.js
+++ index.js
@@ -1,67 +1,135 @@
-const { response } = require('express');
+// This is for creating a server using express
+// Imports
+// Import express from 'express';
 const express = require('express');
-const cors = require('cors');
+// Axios
 const axios = require('axios');
-const app = express();
+const cors = require('cors');
+
 const dotenv = require('dotenv');
 dotenv.config();
+
+const app = express();
+// Run express when this page is rendered/loaded
+
+//const res = require('express/lib/response');
+// Middlewares
+// Middleware for CORS
 app.use(cors());
+// Middleware for JSON data recieving
+app.use(express.json());
 
-const data = [
-  {
-    name  : "Rich",
-    age   : 32,
-    dream : "retired"
-  },
-  {
-    name  : "Drew",
-    age   : 32,
-    dream : "XRPillionaire"
+// File System
+const fs = require('fs');
 
-  },
-  {
-    name  : "Jack",
-    age   : 36,
-    dream : "ReFi Dev"
+// Data
+const beasts = require('./Data/beasts.json');
 
-  }]
+// Routes
+app.get('/',(request,response)=>{
+    
+    response.send('<h1>Hello</h1>');
+    console.log(beasts);
 
-  const reqAPI = 'https://api.sampleapis.com/codingresources/codingResources';
+});
 
-  app.get('/resources', async(req,res)=>{
-    console.log('resources');
-    const reqAPIResult = await axios.get(reqAPI);
-    res.json(reqAPIResult.data);
+// Route 2
+app.get('/v',(req,res)=>{
+
+    res.send('<h1>V is the best </h1>');
+
+});
+
+// Route 3
+app.get('/beasts',(req,res)=>{
+    res.json(beasts);
+});
+
+// Route 4 --> getting beasts by single record --> params
+app.get('/beasts/:id',(req,res)=>{
+
+    const id = parseInt(req.params.id);
+    //console.log(typeof(id));
+
+    //Using array filter method to get th exact data
+    res.send(beasts.filter(items => items.id === id));
+
+});
+
+// Params
+app.get('/beast/:id',(req,res)=>{
+  console.log("***************************");
+  const _id = parseInt(req.params.id);
+  res.send(beasts.filter(items => items.id === _id));
+});
+
+
+// Queries
+app.get('/beasty',(req,res)=>{
+  //http://localhost:8011/beasty?name=something
+  const _name = req.query.name;
+  res.send(beasts.filter(items => items.description === _name));
+});
+
+
+
+// Route 5 --> To get the query info from the URL --> query
+app.get('/beast',(req,res)=>{
+
+    //console.log(req.query);
+    //http://localhost:8011/beast?name=Hydra
+
+    const name = req.query.name;
+    res.send(beasts.filter(item => item.name === name));
+
+
+});
+
+
+// Route 5 --> getting data from external API
+const reqAPI = `https://api.sampleapis.com/wines/reds`;
+
+app.get('/wines', async(req,res)=>{
+  
+  const reqAPIResult = await axios.get(reqAPI);
+  console.log(reqAPIResult.data);
+
+  res.json(reqAPIResult.data);
+  
+});
+
+
+// Route for posting
+app.post('/beasts',(req,res)=>{
+
+  const _id = beasts.length-1;
+
+  // Adding two objects
+  const newDataToBeAddedToBeasts = Object.assign({id:_id},req.body); 
+
+  // Adding to the array
+  beasts.push(newDataToBeAddedToBeasts);
+
+  console.log(typeof(beasts));
+  // Converting the object to an JSON
+  const neArray = JSON.stringify(beasts);
+
+  console.log(neArray);
+  // Methods to write the data
+  // fs.writeFile(arg1,arg2,arg3*);
+  // arg1 is the data path
+  // arg2 is new data to be added
+  // arg3 is a function which gets triggered when the method is called
+  fs.writeFile('./Data/beasts.json',neArray, () =>{
+  res.send(newDataToBeAddedToBeasts);
   })
 
-app.get('/', (request,response)=>{
-  response.send('<h1>Hello World</h1>');
-  console.log(process.env.PORT);
-})
+  // Final step
+  
 
-app.get('/rich', (req, res)=>{
-  res.send('<h1>Rich is awesome</h1>');
-})
+});
 
-app.get('/data', (req, res)=>{
-  res.json(data);
-})
 
-app.listen(8989, ()=>{
-  console.log('http://localhost:8989');
-})
-
-// Get individual item PARAMS
-app.get('/resources/:id', async(req,res)=>{
-  const reqAPIResult = await axios.get(reqAPI);
-  const id = parseInt(req.params.id);
-  res.send(reqAPIResult.data.find(items => items.id === id));
-})
-
-// Get individual item QUERIES
-app.get('/resource', async(req,res)=>{
-  const reqAPIResult = await axios.get(reqAPI);
-  const desc = req.query.description;
-  console.log(reqAPIResult.data.filter(items => items.description === desc))
-  res.send(reqAPIResult.data.filter(items => items.description === desc));
-})
+app.listen(8011,()=>{
+    console.log('http://localhost:8011');
+});
---
+++ server.js
@@ -0,0 +1,67 @@
+const { response } = require('express');
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+app.use(cors());
+
+const data = [
+  {
+    name  : "Rich",
+    age   : 32,
+    dream : "retired"
+  },
+  {
+    name  : "Drew",
+    age   : 32,
+    dream : "XRPillionaire"
+
+  },
+  {
+    name  : "Jack",
+    age   : 36,
+    dream : "ReFi Dev"
+
+  }]
+
+  const reqAPI = 'https://api.sampleapis.com/codingresources/codingResources';
+
+  app.get('/resources', async(req,res)=>{
+    console.log('resources');
+    const reqAPIResult = await axios.get(reqAPI);
+    res.json(reqAPIResult.data);
+  })
+
+app.get('/', (request,response)=>{
+  response.send('<h1>Hello World</h1>');
+  console.log(process.env.PORT);
+})
+
+app.get('/rich', (req, res)=>{
+  res.send('<h1>Rich is awesome</h1>');
+})
+
+app.get('/data', (req, res)=>{
+  res.json(data);
+})
+
+app.listen(8989, ()=>{
+  console.log('http://localhost:8989');
+})
+
+// Get individual item PARAMS
+app.get('/resources/:id', async(req,res)=>{
+  const reqAPIResult = await axios.get(reqAPI);
+  const id = parseInt(req.params.id);
+  res.send(reqAPIResult.data.find(items => items.id === id));
+})
+
+// Get individual item QUERIES
+app.get('/resource', async(req,res)=>{
+  const reqAPIResult = await axios.get(reqAPI);
+  const desc = req.query.description;
+  console.log(reqAPIResult.data.filter(items => items.description === desc))
+  res.send(reqAPIResult.data.filter(items => items.description === desc));
+})
